953272bad33f39d0fe27d5d31dd22b9c83e6d95d
Aaditya Dhruv <asdhruv2@illinois.edu>
2019-11-23 21:25:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cheekychuchu/ChoosingActivity.java
@@ -0,0 +1,165 @@
+package com.example.cheekychuchu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class ChoosingActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
+    private GoogleMap gmap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choosing);
+        ActivityCompat.requestPermissions(ChoosingActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(ChoosingActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Your location is required to play the game!", Toast.LENGTH_SHORT).show();
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(ChoosingActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(ChoosingActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(ChoosingActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+        Button go = findViewById(R.id.goButton);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                CameraPosition position = gmap.getCameraPosition();
+                float bearing = position.bearing;
+                LatLng target = position.target;
+                double lat = target.latitude;
+                double lon = target.longitude;
+                float tilt = position.tilt;
+                float zoom = position.zoom;
+                intent.putExtra("bearing", bearing);
+                intent.putExtra("Lat", lat);
+                intent.putExtra("Lon", lon);
+                intent.putExtra("tilt", tilt);
+                intent.putExtra("zoom", zoom);
+                Toast.makeText(ChoosingActivity.this, position.toString(), Toast.LENGTH_SHORT).show();
+                Log.e("Position", position.toString());
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "AAAAA", Toast.LENGTH_SHORT).show();
+        gmap = googleMap;
+        gmap.setMinZoomPreference(16.8f);
+        gmap.setMaxZoomPreference(17.2f);
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Toast.makeText(ChoosingActivity.this, "h8fw", Toast.LENGTH_SHORT).show();
+                if (location != null) {
+                    Toast.makeText(ChoosingActivity.this, "YEAHHh", Toast.LENGTH_SHORT).show();
+                    gmap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+                    gmap.setMyLocationEnabled(true);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/cheekychuchu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cheekychuchu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.maps.model.CameraPosition;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button play = findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChoosingActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
