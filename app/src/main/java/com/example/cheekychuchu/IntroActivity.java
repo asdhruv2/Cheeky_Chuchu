9d1aa4639dc5f4ee2daf9fa21cd860920b679fff
bhavesh4 <bhavesh4@illinois.edu>
2019-12-09 17:15:15 -0600

    Changed and added some comments to it
    Signed-off-by: bhavesh4 <bhavesh4@illinois.edu>

--- app/src/main/java/com/example/cheekychuchu/IntroActivity.java
+++ app/src/main/java/com/example/cheekychuchu/IntroActivity.java
@@ -18,33 +18,18 @@ public class IntroActivity extends AppIntro2 {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Note here that we DO NOT use setContentView();
-
-        // Add your slide fragments here.
-        // AppIntro will automatically generate the dots indicator and buttons.
-        // Instead of fragments, you can also use our default slide.
-        // Just create a `SliderPage` and provide title, description, background and image.
-        // AppIntro will do the rest.
         SliderPage sliderPage = new SliderPage();
         sliderPage.setTitle("Welcome to Cheeky Chuchu");
         sliderPage.setImageDrawable(R.drawable.paw);
         sliderPage.setDescription("This game will attempt to catch the cutest living being on the planet Earth");
         sliderPage.setBgColor(Color.parseColor("#E16E3C"));
         addSlide(AppIntroFragment.newInstance(sliderPage));
-        // OPTIONAL METHODS
-        // Override bar/separator color.
-        setBarColor(Color.parseColor("#3F51B5"));
-        //setSeparatorColor(Color.parseColor("#2196F3"));
 
-        // Hide Skip/Done button.
+        setBarColor(Color.parseColor("#3F51B5"));
+
         showSkipButton(true);
         setProgressButtonEnabled(true);
-        //setFlowAnimation();
-        // Turn vibration on and set intensity.
-        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
-        //setVibrate(true);
-        //setVibrateIntensity(30);
+        
         SliderPage sliderPage1 = new SliderPage();
         sliderPage1.setTitle("Rules");
         sliderPage1.setImageDrawable(R.drawable.dog);
