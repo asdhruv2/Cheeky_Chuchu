797c3b677fcff7a8c91d00ed65b71f6589b3b8cd
Aaditya Dhruv <asdhruv2@illinois.edu>
2019-12-08 19:36:49 -0600

    Working code TODO:
    Make sure chuchu is not clickable.

--- app/src/main/java/com/example/cheekychuchu/GameActivity.java
+++ app/src/main/java/com/example/cheekychuchu/GameActivity.java
@@ -57,6 +57,12 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
          mMap.getUiSettings().setAllGesturesEnabled(false);
+         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+             @Override
+             public boolean onMarkerClick(Marker marker) {
+                 return false;
+             }
+         });
          Intent intent = getIntent();
          float bearing = intent.getFloatExtra("bearing", 0);
          double lat = intent.getDoubleExtra("Lat", 0);
@@ -73,6 +79,7 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
 
         LatLng topRight =
                 mMap.getProjection().getVisibleRegion().farRight;
+
         double top = topRight.latitude;
         double bottom = bottomLeft.latitude;
         double right = topRight.longitude;
--- app/src/main/java/com/example/cheekychuchu/MainActivity.java
+++ app/src/main/java/com/example/cheekychuchu/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.cheekychuchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.CameraPosition;
 
@@ -19,11 +22,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(getApplicationContext(), IntroActivity.class);
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            //show start activity
+
+            startActivity(new Intent(MainActivity.this, ChoosingActivity.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
+        //startActivity(i);
         Button play = findViewById(R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ChoosingActivity.class);
                 startActivity(intent);
 
             }
